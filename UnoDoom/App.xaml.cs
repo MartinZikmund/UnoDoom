2c16939b1f467487d12162b1231845e823b1f615
Martin Zikmund <martin@zikmund.dev>
2026-01-02 17:11:51 +0100

    More logging

--- UnoDoom/App.xaml.cs
+++ UnoDoom/App.xaml.cs
@@ -88,12 +88,12 @@ public partial class App : Application
 #endif
 
             // Exclude logs below this level
-            builder.SetMinimumLevel(LogLevel.Information);
+            builder.SetMinimumLevel(LogLevel.Debug);
 
             // Default filters for Uno Platform namespaces
-            builder.AddFilter("Uno", LogLevel.Warning);
-            builder.AddFilter("Windows", LogLevel.Warning);
-            builder.AddFilter("Microsoft", LogLevel.Warning);
+            builder.AddFilter("Uno", LogLevel.Debug);
+            builder.AddFilter("Windows", LogLevel.Debug);
+            builder.AddFilter("Microsoft", LogLevel.Debug);
 
             // Generic Xaml events
             // builder.AddFilter("Microsoft.UI.Xaml", LogLevel.Debug );
